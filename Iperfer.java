a96440426ee10bc031265cdf4c4670bf14b553d2
xrayzhang <37488371+xrayzhang@users.noreply.github.com>
2018-09-20 11:27:56 -0500

    part 1
    take in user input from command line. creates socket for server/client i think

---
+++ Iperfer.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.net.*;
+
+public class Iperfer {
+    static boolean clientMode;
+    static boolean serverMode;
+    static String hostName;
+    static int port;
+    static int time;
+    //return code  0 = success
+    //return code -1 = missing / additional args
+    //return code -2 = server port is out of bounds
+    //return code -3 = time is less than 0w
+    public static int readCommands(String[] arguments) {
+        for (int i = 0; i < arguments.length; i++) {
+            if (clientMode && arguments.length != 7) {
+                return -1;
+            }
+            else if (serverMode && arguments.length != 3) {
+                return -1;
+            }
+
+            if (arguments[i].equals("-c")) {
+                clientMode = true;
+            }
+            else if (arguments[i].equals("-s")) {
+                serverMode = true;
+            }
+            else if (arguments[i].equals("-h")) {
+                if (++i >= arguments.length) {
+                    break;
+                }
+                hostName = arguments[i];
+            }
+            else if (arguments[i].equals("-p")) {
+                if (++i >= arguments.length) {
+                    break;
+                }
+                int portNum = Integer.parseInt(arguments[i]);
+                if (portNum < 1024 || portNum > 65535) {
+                    return -2;
+                }
+                port = portNum;
+            }
+            else if (clientMode == true && arguments[i].equals("-t")) {
+                if (++i >= arguments.length) {
+                    break;
+                }
+                time = Integer.parseInt(arguments[i]);
+                if (time < 0) {
+                    return -3;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int success = readCommands(args);
+        if (success == -1) {
+            System.out.println("Error: missing or additional arguments");
+        }
+        else if (success == -2) {
+            System.out.println("Error: port number must be in the range of 1024 to 65535");
+        }
+        else if (success == -3) {
+            System.out.println("Error: time must be greater than 0");
+        }
+        if (success < 0) {
+            System.exit(0);
+        }
+
+        try (
+            Socket echoSocket = new Socket(hostName, port);
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get i/o for the connection to " + hostName);
+            System.exit(1);
+
+        }
+    }
+}
